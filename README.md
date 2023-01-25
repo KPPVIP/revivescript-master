5ad5a5c7a95bb0d78ca545bd2d8abfe0b6d33a3d
Guudz <Kilian1702@outlook.com>
2023-01-25 01:13:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# revivescript (Old and unmaintained project)
+Lua script for FiveM | Revive your self at EMS station
+
+## Requirements:
+- es_extended
+- esx_ambulancejob
+
+## Developers:
+- itIsMaku & Svejdarpa
---
+++ config.lua
@@ -0,0 +1,31 @@
+Config              	= {}
+Config.MaxDistance  	= 4.5
+
+Config.Price        	= 200
+Config.AlwaysAllow	= false -- FALSE TO ALWAYS ALLOW, TRUE TO NOT ALWAYS ALLOW
+Config.ServiceCount	= 2 -- IF ABOVE IS TRUE, WHEN THIS AMOUNT OF EMS IS CONNECTED, USERS CAN'T CHECK IN ANYMORE
+Config.GiveSocietyMoney = true
+
+-- Set this only if you have GiveSocietyMoney = true!
+Config.Society 			= 'society_ambulance'
+
+Config.revList = {
+
+	{
+		text = 'Stiskni ~r~E ~w~pro oziveni doktorem.',
+		heading = 157.03,
+		coords  = {x = 321.97, y = -590.64, z = 42.28},
+	},
+	
+	{
+		text = 'Stiskni ~r~E ~w~pro oziveni doktorem.',
+		heading = 310.38,
+		coords = {x = 1830.4, y = 3678.74, z = 33.27},
+	},
+
+	{
+		text = 'Stiskni ~r~E ~w~pro oziveni doktorem.',
+		heading = 194.34,
+		coords = {x = -259.6, y = 6309.38, z = 31.44},
+	},
+}
